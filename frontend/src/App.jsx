db55650ed122050fe133382fab17400e8bcd19e9
Saniya Pathan <saniyapathan6272@gmail.com>
2026-09-19 15:53:47 +0530

    feat: add login button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
+
 import LandingPage from './pages/LandingPage'
-<<<<<<< HEAD
-import GetStarted from './pages/GetStarted'
-=======
 import StudentDashboard from './pages/StudentDashboard'
->>>>>>> origin/main
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-<<<<<<< HEAD
-      <Route path="/get-started" element={<GetStarted />} />
-=======
-      <Route path="/student/dashboard" element ={<StudentDashboard />} />
->>>>>>> origin/main
+      <Route path="/student/dashboard" element={<StudentDashboard />} />
     </Routes>
   )
 }
