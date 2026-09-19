d948b0b25d93abebf46b3885d2387fdec0d83a41
Saniya Pathan <saniyapathan6272@gmail.com>
2026-09-19 14:46:59 +0530

    save landing page changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import GetStarted from './pages/GetStarted'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/get-started" element={<GetStarted />} />
     </Routes>
   )
 }
